0e1b877e890adcd0fb397c94b9270e70e112d535
kiran kumari <108936589+kirankumari202@users.noreply.github.com>
2023-01-09 21:16:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+Reward calculation
+
+A new announcement has been made by the Mayor, the Fair will be on for more than a month. For rewarding customers who actively purchase in the fair, the developers are asked to compute reward points for credit card purchasing. For a small demo implementation, we now compute reward points for VISA card and HP VISA card. The reward points for VISA card is 1% of the spending for all kinds of purchases. For HP Visa card, 10 additional points are given for fuel purchases. Also, include method Overriding for the method computeRewardPoints() which computes the reward points for both types. write a program using the above specification for computing the reward points.
+
+Enter the holder name
+Mahesh
+Enter the CCV number
+9871-9874-4569
+Enter the bill amount
+1000
+Mention the type of spending
+Fuel
+Choose card type
+1.VISA card
+2.HP VISA card
+1
+Holder name:Mahesh
+CCV:9871-9874-4569
+Reward points:10.0
+Do you want to continue?(Y/N)
+N
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Reward-Calculation
+# Reward Calculation Java GUI Project
+ Reward Calculation Java GUI Project
+ 
+![image](https://user-images.githubusercontent.com/61316762/180593226-ff208202-f2ca-4003-bc89-ed9126c5c7b8.png)
+
+![image](https://user-images.githubusercontent.com/61316762/180591737-6f21b8a8-8839-40bd-9f1b-1b23953626d7.png)
+
+![image](https://user-images.githubusercontent.com/61316762/180591758-6992bb30-a6bf-4b1e-aa46-d336551a4b50.png)
+
